ad9b5a0f4add654ed22b0d7a4e10ea77042847a2
Olaf Willocx <olaf@usa.com>
2021-10-27 23:18:28 +0200

    completed basic depth and time evaluation

--- src/solver_sine.cc
+++ src/solver_sine.cc
@@ -5,9 +5,9 @@ int main()
 {
     //  independent settings
 
-    std::array<size_t, 2> n     {1000, 1000};       //  real space dims
-    std::array<double, 3> a     {3e-7, 1e-7, 1e-7}; //  heat diffusivities (x, y, z)
-    std::array<double, 4> range {0, .1, 0, .1};     //  real space range (x, y)
+    array<size_t, 2> n     {1000, 1000};       //  real space dims
+    array<double, 3> a     {3e-7, 1e-7, 1e-7}; //  heat diffusivities (x, y, z)
+    array<double, 4> range {0, .1, 0, .1};     //  real space range (x, y)
     double b_z   = .1,   //  heat conductivity along z (related to but not independent of a[2])
            d     = 2e-3, //  depth; real space z range is [0, d)
            z_rel = .02,  //  evaluation relative depth
@@ -18,67 +18,94 @@ int main()
 
     //  dependent settings
 
-    std::array<size_t, 2> n_dft {n[0] / 2 + 1, n[1]}; //  frequency space dims
     double z = z_rel * d; //  evaluation depth
-    std::array<double, 2> range_size {range[1] - range[0], range[3] - range[2]},               //  size of range
-                          q_center   {.5 * (range[0] + range[1]), .5 * (range[2] + range[3])}; //  laser dot location (x, y)
+    array<double, 2> q_center {.5 * (range[0] + range[1]), .5 * (range[2] + range[3])}; //  laser dot location (x, y)
 
 
 
     //  space and frequency space coordinates
 
-    double *x   = new double[n[0]],     *y   = new double[n[1]],
-           *k_x = new double[n_dft[0]], *k_y = new double[n_dft[1]];
-    linear_fill(x, n[0], range[0], range_size[0] / n[0]);
-    linear_fill(y, n[1], range[2], range_size[1] / n[1]);
-    linear_fill(k_x, n_dft[0], 0,                           1 / range_size[0]);
-    linear_fill(k_y, n_dft[1], -.5 * (n[1] - 1) / range[2], 1 / range_size[1]);
+    double *x   = new double[n[0]], *y   = new double[n[1]],
+           *k_x = new double[n[0]], *k_y = new double[n[1]];
+    eval_coords_real_space(x,   y,   n, range);
+    eval_coords_freq_space(k_x, k_y, n, range);
 
 
 
     //  real space Gaussian intensity
 
-    double *q = new double[n[0] * n[1]];
-    gaussian_intensity(q, n, x, y, q_P, q_center, q_sd);
+    complex<double> *q = new complex<double>[n[0] * n[1]];
+    eval_intensity_real_space(q, n, x, y, q_P, q_center, q_sd);
     delete[] x;
     delete[] y;
 
+    //  frequency space Gaussian intensity
 
-
-    //  frequency space Gaussian intensity (via real to complex half FFT)
-
-    //  the first FFT along x is real to complex with no negative output frequencies, so only half memory is needed
-    std::complex<double> *q_dft = new std::complex<double>[n_dft[0] * n_dft[1]];
-    fftw_plan plan = fftw_plan_dft_r2c_2d(n[0], n[1], q, reinterpret_cast<fftw_complex *>(q_dft), FFTW_ESTIMATE);
-    fftw_execute(plan);
-    fftw_destroy_plan(plan);
+    complex<double> *q_dft = new complex<double>[n[0] * n[1]];
+    eval_intensity_freq_space(q_dft, n, q);
     delete[] q;
-    //  renormalization
-    for (size_t i = 0; i < n_dft[0] * n_dft[1]; i++)
-        q_dft[i] /= n[0] * n[1];
 
 
 
     //  sigma for frequencies 0 and 1 / q_p
 
     //  s storage format:
-    //      freq 0       pos k_x[0] k_y[0],            ..., freq 0       pos k_x[n_dft[0] - 1] k_y[0],
+    //      freq 0       pos k_x[0] k_y[0],        ..., freq 0       pos k_x[n[0] - 1] k_y[0],
     //      ...,
-    //      freq 0       pos k_x[0] k_y[n_dft[1] - 1], ..., freq 0       pos k_x[n_dft[0] - 1] k_y[n_dft[1] - 1],
-    //      freq 1 / q_p pos k_x[0] k_y[0],            ..., freq 1 / q_p pos k_x[n_dft[0] - 1] k_y[0],
+    //      freq 0       pos k_x[0] k_y[n[1] - 1], ..., freq 0       pos k_x[n[0] - 1] k_y[n[1] - 1],
+    //      freq 1 / q_p pos k_x[0] k_y[0],        ..., freq 1 / q_p pos k_x[n[0] - 1] k_y[0],
     //      ...,
-    //      freq 1 / q_p pos k_x[0] k_y[n_dft[1] - 1], ..., freq 1 / q_p pos k_x[n_dft[0] - 1] k_y[n_dft[1] - 1],
-    std::complex<double> *s = new std::complex<double>[n_dft[0] * n_dft[1] * 2];
-    s_0(s                      , n_dft, k_x, k_y, a);
-    s_1(s + n_dft[0] * n_dft[1], n_dft, k_x, k_y, a, q_p);
+    //      freq 1 / q_p pos k_x[0] k_y[n[1] - 1], ..., freq 1 / q_p pos k_x[n[0] - 1] k_y[n[1] - 1]
+    complex<double> *s = new complex<double>[n[0] * n[1] * 2];
+    eval_sigma_0(s              , n, k_x, k_y, a);
+    eval_sigma_1(s + n[0] * n[1], n, k_x, k_y, a, q_p);
+    delete[] k_x;
+    delete[] k_y;
+
+
+
+    //  time mutual T components for frequencies 0 and 1 / q_p in frequency space
+
+    //  m_T_dft storage format:
+    //      freq 0       pos k_x[0] k_y[0],        ..., freq 0       pos k_x[n[0] - 1] k_y[0],
+    //      ...,
+    //      freq 0       pos k_x[0] k_y[n[1] - 1], ..., freq 0       pos k_x[n[0] - 1] k_y[n[1] - 1],
+    //      freq 1 / q_p pos k_x[0] k_y[0],        ..., freq 1 / q_p pos k_x[n[0] - 1] k_y[0],
+    //      ...,
+    //      freq 1 / q_p pos k_x[0] k_y[n[1] - 1], ..., freq 1 / q_p pos k_x[n[0] - 1] k_y[n[1] - 1]
+    complex<double> *m_T_dft = new complex<double>[n[0] * n[1] * 2];
+    eval_time_mutual_T_freq_space(m_T_dft,               n, q_dft, s,               b_z, z, d);
+    eval_time_mutual_T_freq_space(m_T_dft + n[0] * n[1], n, q_dft, s + n[0] * n[1], b_z, z, d);
+    delete[] s;
+    delete[] q_dft;
+
+
+
+    //  time mutual T components for frequencies 0 and 1 / q_p in real space
+
+    //  m_T storage format:
+    //      freq 0       pos x[0] y[0],        ..., freq 0       pos x[n[0] - 1] y[0],
+    //      ...,
+    //      freq 0       pos x[0] y[n[1] - 1], ..., freq 0       pos x[n[0] - 1] y[n[1] - 1],
+    //      freq 1 / q_p pos x[0] y[0],        ..., freq 1 / q_p pos x[n[0] - 1] y[0],
+    //      ...,
+    //      freq 1 / q_p pos x[0] y[n[1] - 1], ..., freq 1 / q_p pos x[n[0] - 1] y[n[1] - 1]
+    complex<double> *m_T = new complex<double>[n[0] * n[1] * 2];
+    eval_time_mutual_T_real_space(m_T,               m_T_dft,               n);
+    eval_time_mutual_T_real_space(m_T + n[0] * n[1], m_T_dft + n[0] * n[1], n);
+    delete[] m_T_dft;
+
+
+
+    //  T in real space
+
+    complex<double> *T = new complex<double>[n[0] * n[1]];
+    eval_T(T, m_T, n, q_p, t);
+    delete[] m_T;
 
 
 
     //  storage
 
-    store(static_cast<void *>(q_dft), {n_dft[0], n_dft[1], 1}, "0.vti");
-    delete[] k_x;
-    delete[] k_y;
-    delete[] q_dft;
-    delete[] s;
+    store(static_cast<void *>(T), {n[0], n[1], 1}, "0.vti");
 }
--- src/solver_sine.hh
+++ src/solver_sine.hh
@@ -3,14 +3,30 @@
 #include <complex>
 #include <fftw3.h>
 
+using std::array, std::complex;
+
 void linear_fill(double *data, size_t n, double begin, double step)
 {   for (size_t i = 0; i < n; i++)
     {   data[i] = begin + step * i;
     }
 }
 
-void gaussian_intensity(double *q, std::array<size_t, 2> n, double *x, double *y,
-                        double q_P, std::array<double, 2> q_center, double q_sd)
+
+
+void eval_coords_real_space(double *x, double *y, array<size_t, 2> n, array<double, 4> range)
+{   linear_fill(x, n[0], range[0], (range[1] - range[0]) / n[0]);
+    linear_fill(y, n[1], range[2], (range[3] - range[2]) / n[1]);
+}
+
+void eval_coords_freq_space(double *k_x, double *k_y, array<size_t, 2> n, array<double, 4> range)
+{   linear_fill(k_x, n[0], 0,                               1 / (range[1] - range[0]));
+    linear_fill(k_y, n[1], -.5 * (n[1] - 1) / (range[3] - range[2]), 1 / (range[3] - range[2]));
+}
+
+
+
+void eval_intensity_real_space(complex<double> *q, array<size_t, 2> n, double *x, double *y,
+                               double q_P, array<double, 2> q_center, double q_sd)
 {   double a = .5 * M_1_PI * q_P / (q_sd * q_sd);
     for (size_t j = 0; j < n[1]; j++)
         for (size_t i = 0; i < n[0]; i++)
@@ -20,17 +36,62 @@ void gaussian_intensity(double *q, std::array<size_t, 2> n, double *x, double *y
         }
 }
 
-void s_0(std::complex<double> *s, std::array<size_t, 2> n_dft, double *k_x, double *k_y, std::array<double, 3> a)
-{   for (size_t j = 0; j < n_dft[1]; j++)
-        for (size_t i = 0; i < n_dft[0]; i++)
-            s[i + j * n_dft[0]] = 2 * M_PI * sqrt((a[0] * (k_x[i] * k_x[i])
-                                                 + a[1] * (k_y[j] * k_y[j])) / a[2]);
+void eval_intensity_freq_space(complex<double> *q_dft, array<size_t, 2> n, complex<double> *q)
+{   fftw_plan plan = fftw_plan_dft_2d(n[0], n[1],
+                                      reinterpret_cast<fftw_complex *>(q), reinterpret_cast<fftw_complex *>(q_dft),
+                                      FFTW_FORWARD, FFTW_ESTIMATE);
+    fftw_execute(plan);
+    fftw_destroy_plan(plan);
+    for (size_t i = 0; i < n[0] * n[1]; i++)
+        q_dft[i] /= n[0] * n[1];
 }
 
-void s_1(std::complex<double> *s, std::array<size_t, 2> n_dft, double *k_x, double *k_y, std::array<double, 3> a, double q_p)
+
+
+void eval_sigma_0(complex<double> *s, array<size_t, 2> n, double *k_x, double *k_y, array<double, 3> a)
+{   for (size_t j = 0; j < n[1]; j++)
+        for (size_t i = 0; i < n[0]; i++)
+            s[i + j * n[0]] = 2 * M_PI * sqrt((a[0] * (k_x[i] * k_x[i])
+                                             + a[1] * (k_y[j] * k_y[j])) / a[2]);
+}
+
+void eval_sigma_1(complex<double> *s, array<size_t, 2> n, double *k_x, double *k_y, array<double, 3> a, double q_p)
 {   using namespace std::complex_literals;
-    for (size_t j = 0; j < n_dft[1]; j++)
-        for (size_t i = 0; i < n_dft[0]; i++)
-            s[i + j * n_dft[0]] = sqrt(2i * M_PI) * sqrt((1i / q_p + 2 * M_PI * a[0] * k_x[i] * k_x[i]
-                                                                   + 2 * M_PI * a[1] * k_y[j] * k_y[j]) / a[2]);
+    for (size_t j = 0; j < n[1]; j++)
+        for (size_t i = 0; i < n[0]; i++)
+            s[i + j * n[0]] = sqrt(2i * M_PI) * sqrt((1i / q_p + 2 * M_PI * a[0] * k_x[i] * k_x[i]
+                                                               + 2 * M_PI * a[1] * k_y[j] * k_y[j]) / a[2]);
+}
+
+
+
+void eval_time_mutual_T_freq_space(complex<double> *m_T_dft, array<size_t, 2> n, complex<double> *q_dft, complex<double> *s,
+                                   double b_z, double z, double d)
+{   for (size_t i = 0; i < n[0] * n[1]; i++)
+    {   complex<double> a = q_dft[i] / (b_z * s[i]),
+                        b = z * s[i],
+                        c  = 2 * d * s[i];
+        //  the below approximates a * (exp(b) / (1. - exp(c)) - exp(-b) / (1. - exp(-c))), which is too unstable due to large c
+        m_T_dft[i] = a * (-exp(b - c) - exp(-b) / (1. - exp(-c)));
+    }
+}
+
+void eval_time_mutual_T_real_space(complex<double> *m_T, complex<double> *m_T_dft, array<size_t, 2> n)
+{   fftw_plan plan = fftw_plan_dft_2d(n[1], n[0],
+                                      reinterpret_cast<fftw_complex *>(m_T_dft), reinterpret_cast<fftw_complex *>(m_T),
+                                      FFTW_BACKWARD, FFTW_ESTIMATE);
+    fftw_execute(plan);
+    fftw_destroy_plan(plan);
+    for (size_t i = 0; i < n[0] * n[1]; i++)
+        m_T[i] *= n[0] * n[1];
+}
+
+
+
+void eval_T(complex<double> *T, complex<double> *m_T, array<size_t, 2> n, double q_p, double t)
+{   using namespace std::complex_literals;
+    complex<double> *m_T_0 = m_T,
+                    *m_T_1 = m_T + n[0] * n[1];
+    for (size_t i = 0; i < n[0] * n[1]; i++)
+        T[i] = .5 * (m_T_0[i] + m_T_1[i] * exp(2i * M_PI * t / q_p));
 }
